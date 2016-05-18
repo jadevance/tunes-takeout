0aae5b03ccd464e663d289aa3772bf957ac267fd
Jade Vance <jadenicolevance@gmail.com>
2016-05-18 05:34:34 -0700

    added controllers and login functionality

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,5 +1,12 @@
 class SuggestionsController < ApplicationController
+  skip_before_action :require_login, only: :index
 
   def index
+
   end
+
+  def new
+    @suggestions = Food.new
+  end
+  
 end
