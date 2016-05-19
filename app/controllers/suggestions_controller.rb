425bb1fb6300702b4fec9e8ed3cb9ea5461c4816
Jade Vance <jadenicolevance@gmail.com>
2016-05-18 23:15:17 -0700

    clarified code in the models, finished writing the food model to query yelp api

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -2,11 +2,31 @@ class SuggestionsController < ApplicationController
   skip_before_action :require_login, only: :index
 
   def index
+    if @suggestions.nil?
+      redirect_to root_path, notice: "No matches for #{params["query"]}!"
+    else
+      render :index
+    end
+  end 
 
+  def create
+    @tunes_takeout = TunesTakeoutWrapper.search(params["query"])
+    @suggestions = @tunes_takeout.suggestions
+    @music_suggestions = Music.suggested_music(@suggestions)
+    @food_suggestions = Food.suggested_food(@suggestions)
+    render :index 
+  end 
+
+
+  def show
+    # individual restaurants / music 
+    # don't forget a route
+  end 
+
+  def favorite
   end
 
-  def new
-    @suggestions = Food.new
-  end
-  
+  def unfavorite
+  end 
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,37 @@
 class UsersController < ApplicationController
-  def create
-    auth_hash = request.env['omniauth.auth']
 
-    if auth_hash["info"]["id"]
-      @user = User.find_or_create_from_omniauth(auth_hash)
-      if @user
-        session[:user_id] = @user.id
-        redirect_to root_path
-      else
-        redirect_to root_path, notice: "Failed to save the user"
-      end
-    else
-      redirect_to root_path, notice: "Failed to authenticate"
-    end
-  end
+    def spotify
+    spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
+    # Now you can access user's private data, create playlists and much more
 
+    # Access private data
+    spotify_user.country #=> "US"
+    spotify_user.email   #=> "example@email.com"
 
-  def destroy
-    session.delete :user_id
-    redirect_to root_path
+    # Add tracks to a playlist in user's Spotify account
+    tracks = RSpotify::Track.search('Know')
+    playlist.add_tracks!(tracks)
+    playlist.tracks.first.name #=> "Somebody That I Used To Know"
+
+    # Access and modify user's music library
+    spotify_user.save_tracks!(tracks)
+    spotify_user.saved_tracks.size #=> 20
+    spotify_user.remove_tracks!(tracks)
+
+    albums = RSpotify::Album.search('launeddas')
+    spotify_user.save_albums!(albums)
+    spotify_user.saved_albums.size #=> 10
+    spotify_user.remove_albums!(albums)
+
+    # Use Spotify Follow features
+    spotify_user.follow(playlist)
+    spotify_user.follows?(artists)
+    spotify_user.unfollow(users)
+
+    # Get user's top played artists and tracks
+    spotify_user.top_artists #=> (Artist array)
+    spotify_user.top_tracks(time_range: 'short_term') #=> (Track array)
+
+    # Check doc for more
   end
 end
