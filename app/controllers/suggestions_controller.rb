30c7007feaa6c185843482ec8002b93e0e1145b1
Jade Vance <jadenicolevance@gmail.com>
2016-05-20 10:50:20 -0700

    Music and Food are now paired together, yaaaay

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,7 +1,7 @@
 class SuggestionsController < ApplicationController
   skip_before_action :require_login, only: :index
 
-  def index
+  def index    
     if @suggestions.nil?
       redirect_to root_path, notice: "No matches for #{params["query"]}!"
     else
@@ -15,14 +15,27 @@ class SuggestionsController < ApplicationController
     
     @music_suggestions  = Music.suggested_music(@suggestions)
     @food_suggestions   = Food.suggested_food(@suggestions)
-    
+    # lol I heart zip
+    @pairing_suggestions = @music_suggestions.zip(@food_suggestions) 
     render :index 
   end 
 
-  def favorite
+  def favorites
+    # get the favorites, array of ids
+    @top_favorites   = TunesTakeoutWrapper.top_favorites 
+    list = []
+    #look up each favorite 
+    @top_favorites.each do |id|
+      @tunes_takeout = TunesTakeoutWrapper.each_favorite(id) 
+      # list of urls in an array 
+      list << @tunes_takeout.href
+    end 
   end
 
   def unfavorite
   end 
 
 end
+
+# favorites right now goes to a top ten list
+# need to think about favorites by user 
