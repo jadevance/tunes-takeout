e8fc2c234328f4a3c51dc3016e6905ca369d92dd
Jade Vance <jadenicolevance@gmail.com>
2016-05-20 12:40:43 -0700

    extracting tunes and takeout ids from suggestions, not doing anything with them yet. Need to associate them with pairings

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -13,22 +13,31 @@ class SuggestionsController < ApplicationController
     @tunes_takeout      = TunesTakeoutWrapper.search(params["query"]) 
     @suggestions        = @tunes_takeout.suggestions
     
+    #extract Tunes and Takeout ids from suggestions
+    @ids = []
+    @suggestions.each do |suggestion|
+      @ids << suggestion["id"]
+    end  
+
     @music_suggestions  = Music.suggested_music(@suggestions)
     @food_suggestions   = Food.suggested_food(@suggestions)
-    # lol I heart zip
+    
+    # lol I heart doing janky zip stuff
     @pairing_suggestions = @music_suggestions.zip(@food_suggestions) 
     render :index 
   end 
 
   def favorites
-    # get the favorites, array of ids
-    @top_favorites   = TunesTakeoutWrapper.top_favorites 
-    list = []
+    # get the favorites
+    @tunes_takeout   = TunesTakeoutWrapper.top_favorites 
+    # array of tunestakeout ids
+    @top_favorites =  @tunes_takeout.suggestions
+    
+    @list = []
     #look up each favorite 
     @top_favorites.each do |id|
-      @tunes_takeout = TunesTakeoutWrapper.each_favorite(id) 
-      # list of urls in an array 
-      list << @tunes_takeout.href
+      # return a hash with each id, food_id and music_id
+      @list << TunesTakeoutWrapper.each_favorite(id) 
     end 
   end
 
