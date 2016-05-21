eb65c26d8d620875ccd231f499d2d55ec4cdb945
Jade Vance <jadenicolevance@gmail.com>
2016-05-20 22:48:09 -0700

    Played with javascript amd made a favorite heart that toggles between empty and filled.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+$(function() {
+    $('a').click(function() {
+        $(this).find('i').toggleClass('fa fa-heart-o fa fa-heart');
+    });
+});
